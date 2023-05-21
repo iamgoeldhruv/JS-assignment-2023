17a62a6743856f0f5ac42e15e6b9239fc0f300ba
RoopamTaneja <roopamtaneja999@gmail.com>
2023-05-21 12:36:04 +0530

    Added dice

--- game/script.js
+++ game/script.js
@@ -17,7 +17,47 @@ function setBox(id, color) {
     document.getElementById(id).innerHTML = `<div class ="cir" style = "background:${color}"></div>`
 }
 
+function clearBox(id) {
+    document.getElementById(id).innerHTML = ``;
+}
+
 
 createBoxes();
-setBox("b_40", "yellow");
-setBox("b_41", "blueviolet");
+// setBox("b_40", "yellow");
+// setBox("b_41", "rgb(12, 12, 181)");
+
+var move = 5;
+var pos = 0;
+
+function playerMover(move) {
+    if (move > 0) {
+        setTimeout(() => {
+            if (pos >= 1) clearBox("b_" + pos);
+            pos++;
+            setBox("b_" + pos, "yellow");
+            move--;
+            playerMover(move)
+        }, 300)
+    }
+}
+
+// playerMover(move);
+
+//Dice Area:
+let images = ["img/dice-01.svg", "img/dice-02.svg", "img/dice-03.svg", "img/dice-04.svg", "img/dice-05.svg", "img/dice-06.svg"];
+let dice = document.querySelectorAll('.dice');
+
+function roll() {
+    dice.forEach(function (die) {
+        die.classList.add("shake")
+    });
+    setTimeout(function () {
+        dice.forEach(function (die) {
+            die.classList.remove("shake")
+        });
+        let dieVal1 = Math.floor((Math.random()) * 6);
+        document.querySelector('#dice-01').setAttribute('src', images[dieVal1]);
+    }, 500)
+}
+
+document.querySelector('.btn').addEventListener('click', roll);
