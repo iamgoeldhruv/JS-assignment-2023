0aec5abb7490d8a0bde928625c6c88c1ee63cff9
RoopamTaneja <roopamtaneja999@gmail.com>
2023-05-21 14:15:58 +0530

    Added two players logic

--- game/script.js
+++ game/script.js
@@ -13,6 +13,8 @@ function createBoxes() {
     document.querySelector(".board").innerHTML = boxes;
 }
 
+createBoxes();
+
 function setBox(id, color) {
     document.getElementById(id).innerHTML = `<div class ="cir" style = "background:${color}"></div>`
 }
@@ -21,40 +23,59 @@ function clearBox(id) {
     document.getElementById(id).innerHTML = ``;
 }
 
+var pos1 = 0;
+var pos2 = 0;
 
-createBoxes();
-// setBox("b_40", "yellow");
-// setBox("b_41", "rgb(12, 12, 181)");
-
-var move = 5;
-var pos = 0;
-
-function playerMover(move) {
+function player1Mover(move) {
     if (move > 0) {
         setTimeout(() => {
-            if (pos >= 1) clearBox("b_" + pos);
-            pos++;
-            setBox("b_" + pos, "yellow");
+            if (pos1 >= 1) { clearBox("b_" + pos1); }
+            pos1++;
+            setBox("b_" + pos1, "yellow");
             move--;
-            playerMover(move)
+            player1Mover(move)
         }, 300)
     }
 }
 
-// playerMover(move);
+function player2Mover(move) {
+    if (move > 0) {
+        setTimeout(() => {
+            if (pos2 >= 1) { clearBox("b_" + pos2); }
+            pos2++;
+            setBox("b_" + pos2, "rgb(12,12,181)");
+            move--;
+            player2Mover(move)
+        }, 300)
+    }
+}
 
 //Dice Area:
 let images = ["img/dice-01.svg", "img/dice-02.svg", "img/dice-03.svg", "img/dice-04.svg", "img/dice-05.svg", "img/dice-06.svg"];
 
-function roll(id) {
+document.querySelector('#btn1').addEventListener('click', () => { roll1('#dice-01') });
+document.querySelector('#btn2').addEventListener('click', () => { roll2('#dice-02') });
+
+function roll1(id) {
     let die = document.querySelector(id);
     die.classList.add("shake")
+    let dieVal1 = Math.floor((Math.random()) * 6);
     setTimeout(function () {
         die.classList.remove("shake");
-        let dieVal = Math.floor((Math.random()) * 6);
-        die.setAttribute('src', images[dieVal]);
+        die.setAttribute('src', images[dieVal1]);
+        let step1 = dieVal1 + 1;
+        player1Mover(step1);
     }, 500)
 }
 
-document.querySelector('#btn1').addEventListener('click', () => { roll('#dice-01') });
-document.querySelector('#btn2').addEventListener('click', () => { roll('#dice-02') });
+function roll2(id) {
+    let die = document.querySelector(id);
+    die.classList.add("shake")
+    let dieVal2 = Math.floor((Math.random()) * 6);
+    setTimeout(function () {
+        die.classList.remove("shake");
+        die.setAttribute('src', images[dieVal2]);
+        let step2 = dieVal2 + 1;
+        player2Mover(step2);
+    }, 500)
+}
