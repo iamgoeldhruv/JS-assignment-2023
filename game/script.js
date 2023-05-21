cc5eadc22db7e55d731a9c8938ab6c0fda5723a0
RoopamTaneja <roopamtaneja999@gmail.com>
2023-05-21 13:35:13 +0530

    Added dice logic

--- game/script.js
+++ game/script.js
@@ -45,19 +45,16 @@ function playerMover(move) {
 
 //Dice Area:
 let images = ["img/dice-01.svg", "img/dice-02.svg", "img/dice-03.svg", "img/dice-04.svg", "img/dice-05.svg", "img/dice-06.svg"];
-let dice = document.querySelectorAll('.dice');
 
-function roll() {
-    dice.forEach(function (die) {
-        die.classList.add("shake")
-    });
+function roll(id) {
+    let die = document.querySelector(id);
+    die.classList.add("shake")
     setTimeout(function () {
-        dice.forEach(function (die) {
-            die.classList.remove("shake")
-        });
-        let dieVal1 = Math.floor((Math.random()) * 6);
-        document.querySelector('#dice-01').setAttribute('src', images[dieVal1]);
+        die.classList.remove("shake");
+        let dieVal = Math.floor((Math.random()) * 6);
+        die.setAttribute('src', images[dieVal]);
     }, 500)
 }
 
-document.querySelector('.btn').addEventListener('click', roll);
+document.querySelector('#btn1').addEventListener('click', () => { roll('#dice-01') });
+document.querySelector('#btn2').addEventListener('click', () => { roll('#dice-02') });
