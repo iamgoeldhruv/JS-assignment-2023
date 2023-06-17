4aa00c1af5f8f346cd6f09bbcd266742d9d32005
DRDiv <divij.rawal@gmail.com>
2023-06-17 20:56:39 +0530

    Added new feature of avatar

--- TicTacToe-Divij/script.js
+++ TicTacToe-Divij/script.js
@@ -1,26 +1,69 @@
+
+
+
 var l = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 var count = 0;
 var mode = false;
-var draw=false;
+var draw = false;
 function createNewElement(source) {
     var element = document.createElement('img');
     element.src = source;
     element.style.height = '8rem';
     return element;
 }
-function showAlert(message) {
+function showAlert(message, icon) {
     const alertBox = document.createElement('div');
     alertBox.className = 'alert';
     alertBox.innerHTML = `
       <div class="alert-content">
+      <img src=${icon}>
         <h2>${message}</h2>
         <button onclick="location.reload()">OK</button>
       </div>
     `;
     document.body.appendChild(alertBox);
-  }
+}
+
+
+function select(inp) {
+    return new Promise((resolve, reject) => {
+        const alertBox = document.createElement('div');
+        alertBox.className = 'alert';
+        alertBox.innerHTML = `
+        <div class="alert-content">
+        <h2>PLEASE SELECT AVATAR FOR ${inp}:</h2>
+          <div class="icon-container">
+
+            <img src="bear.png" alt="bear" onclick="selectImage(this.src)">
+            <img src="cat.png" alt="cat" onclick="selectImage(this.src)">
+            <img src="woman.png" alt="woman" onclick="selectImage(this.src)">
+            <img src="gamer.png" alt="gamer" onclick="selectImage(this.src)">
+            <img src="man.png" alt="man" onclick="selectImage(this.src)">
+          </div>
+          
+        </div>
+      `;
+
+        document.body.appendChild(alertBox);
+
+        window.selectImage = function (src) {
+            delete window.selectImage;
+            alertBox.remove();
+            resolve(src);
+        };
+
+        window.proceed = function () {
+            delete window.proceed;
+            alertBox.remove();
+            reject(new Error('Player canceled selection.'));
+        };
+    });
+}
+
+
 var list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-function check() {
+function check(icon1, icon2) {
+
     bool = false
     m = -1
     if (!(l[0].includes(0) || l[1].includes(0) || (l[2].includes(0)))) {
@@ -72,36 +115,38 @@ function check() {
         bool = true
         m = l[2][0]
         var p = ['r31', 'r22', 'r13']
-        
-    }
 
+    }
+    console.log(bool)
     if (bool) {
-        mode=!mode
+        mode = !mode
         if (p.length == 0 && !draw) {
-            setTimeout(()=>{draw=true;
-                showAlert("DRAW");
-                
-                },30)
-                
-           
-                
-                
-            
+            setTimeout(() => {
+                draw = true;
+                showAlert("DRAW", "computer.png");
+
+            }, 30)
+
+
+
+
+
         }
         else {
             p.forEach(function (element) { document.getElementById(element).children[0].src = (m == 1) ? "redcross.png" : "redzero.png" })
             setTimeout(() => {
                 s = m == 1 ? 'P1 WIN' : 'P2 WIN';
-                showAlert(s)
-               
-                
+                icon = m == 1 ? icon1 : icon2;
+
+                showAlert(s, icon)
+
+
             }, 60)
-            
-        
+
+
         }
-       
-        console.log(draw)
-            console.log(mode)   
+
+
 
 
 
@@ -113,7 +158,7 @@ function getRandomInt(min, max) {
 }
 function willwork(a) {
     let cell = ""
-   
+
     if (l[0][0] == a && l[0][1] == a && l[0][2] == 0) {
         cell = doeswork(0, 2)
 
@@ -191,19 +236,22 @@ function willwork(a) {
     else if (l[2][0] == a && l[0][2] == a && l[1][1] == 0) {
         cell = doeswork(1, 1)
     }
+    else if (list.indexOf(5) != -1 && l[1][1] == 0) {
+        cell = doeswork(1, 1)
+    }
     return cell
 }
 function doeswork(a, b) {
     cell = 'r' + (a + 1) + (b + 1);
     l[a][b] = 2;
- 
+
     list.splice(list.indexOf(3 * a + b), 1)
-    
+
     return cell
 }
 function computermove() {
-    if (!mode)return
-    
+    if (!mode) return
+
     var element = createNewElement('zero.png');
 
     var cell = "";
@@ -229,23 +277,23 @@ function computermove() {
         }
         console.log(ind)
     }
-   
-    
+
+
     if (mode)
-    document.getElementById(cell).appendChild(element);
+        document.getElementById(cell).appendChild(element);
 
 
 }
 
-function handleClick(cell) {
+function handleClick(cell, icon1, icon2) {
 
 
 
     if (mode) {
-        
+
         if (l[parseInt(cell.id[1]) - 1][parseInt(cell.id[2]) - 1] == 0) {
-            
-           var element = createNewElement('cross.png');
+
+            var element = createNewElement('cross.png');
             var int1 = parseInt(cell.id[1]) - 1
             var int2 = parseInt(cell.id[2]) - 1
 
@@ -255,18 +303,22 @@ function handleClick(cell) {
 
             cell.appendChild(element);
             count++;
-            check()
-            if(mode&&!draw)setTimeout(computermove,100)
-    
-            if (!draw && mode)setTimeout(check,150)}
-            
-               
-                
+            check(icon1, icon2)
+            setTimeout(() => {
+                if (mode && !draw) computermove()
+
+                if (!draw && mode) check(icon1, icon2)
+            }, 20)
+
         }
-    
 
 
-    
+
+    }
+
+
+
+
     else {
         if (l[parseInt(cell.id[1]) - 1][parseInt(cell.id[2]) - 1] == 0) {
             var element = count % 2 === 0 ? createNewElement('cross.png') : createNewElement('zero.png');
@@ -274,7 +326,7 @@ function handleClick(cell) {
             cell.appendChild(element);
             count++;
         }
-        setTimeout(check, 5);
+        setTimeout(check(icon1, icon2), 5);
     }
 
 
@@ -282,49 +334,85 @@ function handleClick(cell) {
 
 
 function computer() {
-    if (mode) return;
-    mode = true
-    document.getElementById('players').style.display="none";
-    
-    const tds = document.querySelectorAll('td');
-    count = 0
-    l = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    tds.forEach((td) => {
-        while (td.firstChild) {
-            td.removeChild(td.firstChild);
-        }
-    });
+    var icon1
+    select("PLAYER").then((icon1t) => {
+        icon1 = icon1t
+        icon2 = "computer.png"
+        if (mode) return;
+        mode = true
+        const classPlayers=document.createElement('class')
+        classPlayers.innerHTML=`
+        <div >
+        <h2>PLAYER:<img class="image" src=${icon1}></h2>
+     
+        </div>
+        `
+        headingmain=document.getElementById('heading')
+        headingmain.remove()
+        document.getElementById('computer').style.display = 'none';
+        document.getElementById('players').style.display = 'none';
+        document.getElementById('mode').appendChild(classPlayers)
+        document.getElementById('players').style.display = "none";
+
+        const tds = document.querySelectorAll('td');
+        count = 0
+        l = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        tds.forEach((td) => {
+            while (td.firstChild) {
+                td.removeChild(td.firstChild);
+            }
+        });
+    })
 
     var cells = document.querySelectorAll('td');
 
     cells.forEach(function (cell) {
 
-        cell.addEventListener('click', ()=>(handleClick(cell)));
+        cell.addEventListener('click', () => (handleClick(cell, icon1, icon2)));
     });
 }
 function player2() {
+    var icon1, icon2;
+    select("PLAYER 1").then((icon1t) => {
+        icon1 = icon1t
+        return select("PLAYER 2");
+    }).then((icon2t) => {
+        icon2 = icon2t
 
-    mode = false
-    document.getElementById('computer').style.display="none";
-    count = 0
-    l = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    const tds = document.querySelectorAll('td');
-    tds.forEach((td) => {
-        while (td.firstChild) {
-            td.removeChild(td.firstChild);
-        }
-    });
+        mode = false;
+        const classPlayers=document.createElement('class')
+        classPlayers.innerHTML=`
+        <div >
+        <h2>PLAYER 1:<img class="image" src=${icon1}></h2>
+        <h2>PLAYER 2:<img class="image" src=${icon2}></h2> 
+        </div>
+        `
+        headingmain=document.getElementById('heading')
+        headingmain.remove()
+        document.getElementById('computer').style.display = 'none';
+        document.getElementById('players').style.display = 'none';
+        document.getElementById('mode').appendChild(classPlayers)
+        count = 0;
+        l = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+        const tds = document.querySelectorAll('td');
+        tds.forEach((td) => {
+            while (td.firstChild) {
+                td.removeChild(td.firstChild);
+            }
+        });
 
-
-
-
-    var cells = document.querySelectorAll('td');
-
-    cells.forEach(function (cell) {
-
-        cell.addEventListener('click', () => handleClick(cell));
-    });
-
+        var cells = document.querySelectorAll('td');
+        cells.forEach(function (cell) {
+            cell.addEventListener('click', () => handleClick(cell, icon1, icon2));
+        });
+    })
 }
+
+
+
+
+
+
+
 document.getElementById('players').addEventListener('click', player2)
 document.getElementById('computer').addEventListener('click', computer)
